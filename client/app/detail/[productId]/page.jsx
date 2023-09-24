2e62c82f0704ff5c4be35ee9d5908829cf3c597a
dangminhkhoi2212 <kkhoi600@gmail.com>
2023-09-24 16:07:32 +0700

    :fire: update edit prodcut and add product

--- client/app/detail/[productId]/page.jsx
+++ client/app/detail/[productId]/page.jsx
@@ -1,20 +1,17 @@
-import Carousel from '@/components/ProductDetail/Carousel';
+'use client';
 import { getProductByProductId } from '@/services/product.service';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Detail = ({ params }) => {
     const getProduct = useQuery({
+        queryKey: ['get-detail-product'],
         queryFn: () => {
             return getProductByProductId(params.productId);
         },
     });
-    const images = getProduct.data.images;
-    return (
-        <div>
-            <Carousel images={images} />
-        </div>
-    );
+    const images = getProduct?.data?.images;
+    return <div>{params.productId}</div>;
 };
 
 export default Detail;
