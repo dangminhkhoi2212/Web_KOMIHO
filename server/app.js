5beebe35fe86bc5e1a6dd1be3ac8e66306900cac
dangminhkhoi2212 <kkhoi600@gmail.com>
2023-12-04 21:59:27 +0700

    :fire: update cors

--- server/app.js
+++ server/app.js
@@ -42,7 +42,13 @@ const io = new Server(httpServer, {
     },
 });
 
-app.use(cors());
+app.use(
+    cors({
+        origin: '*',
+        credentials: true, //access-control-allow-credentials:true
+        optionSuccessStatus: 200,
+    }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
